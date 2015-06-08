5dfc6cb6d4b4aea209ace0fff70e2726d97e71a4
Alexandr <diesel594@gmail.com>
2015-06-08 11:50:05 +0300

    Created workMap fill-up method.

--- src/BaseStation.java
+++ src/BaseStation.java
@@ -5,28 +5,4 @@ public class BaseStation extends Equipment{
     public BaseStation(double posX, double posY, int capacity, double price) {
         super(posX, posY, capacity, price);
     }
-    /*private double posX;
-    private double posY;
-    private int capacity;
-    private double price;
-
-    public BaseStation(double posX, double posY, int capacity, double price) {
-
-    }
-
-    public double getPosX(){
-        return this.posX;
-    }
-
-    public double getPosY(){
-        return this.posY;
-    }
-
-    public double getPrice(){
-        return this.price;
-    }
-
-    public int getCapacity(){
-        return this.capacity;
-    }*/
 }
